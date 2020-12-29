4c5832e244316194fe078f9bc78c2a3906a1084b
sp <sam.pack.sp@gmail.com>
2020-12-29 14:27:22 +1030

    trying to get service worker to register

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,4 +1,4 @@
-const FILES_TO_CACHE = ["/", "/index.html", "index.js"/* , "favicon.ico" */];
+const FILES_TO_CACHE = ["/", "index.html", "index.js", "styles.css"/* , "favicon.ico" */];
 
 const CACHE_NAME = "static-cache-v2";
 const DATA_CACHE_NAME = "data-cache-v1";
